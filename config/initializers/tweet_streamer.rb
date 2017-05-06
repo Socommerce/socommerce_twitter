8372b6393071c96c7465d5b1fed907939486b51c
vigneshwaran <vigneshsika@gmail.com>
2017-05-06 12:31:17 +0530

    Add initializer for streaming api

---
+++ config/initializers/tweet_streamer.rb
@@ -0,0 +1,34 @@
+p "working"
+# ENV["RAILS_ENV"] ||= "development"
+# root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+# require File.join(root, 'environment')
+
+# log = File.join(root, 'log', 'stream.log')
+
+TweetStream.configure do |conf|
+  conf.consumer_key        = 'a0UmBU7AHn0fFneQ10zYwP0Kk'
+  conf.consumer_secret     = 'JZEqUgKJsTrYjjvYv1wO4jtrNpy50pwoTUwr4UAro3pPxS7Hhw'
+  conf.oauth_token         = '3139928784-B7kPZz6zLAe1M9bTFI29dSfhU6J6Fi9mNdOmhqC'
+  conf.oauth_token_secret  = 'f6cGgOSBU78IPjHz7lDCiOfv4zS4K90OepO2x2355JFqS'
+  conf.auth_method         = :oauth
+end
+
+# daemon = TweetStream::Daemon.new('tweet_streamer', log_output: true)
+# daemon.on_inited do
+#   ActiveRecord::Base.connection.reconnect!
+#   ActiveRecord::Base.logger = Logger.new(File.open(log, 'a'))
+# end
+# daemon.userstream do |status|
+# 	binding.pry
+#   puts status.text
+# end
+
+client = TweetStream::Client.new	
+# daemon.on_inited do
+#   ActiveRecord::Base.connection.reconnect!
+#   ActiveRecord::Base.logger = Logger.new(File.open(log, 'a'))
+# end
+client.userstream do |status|
+  puts status.text
+  puts JSON.parse(status)
+end
--- lib/tweet_streamer.rb
+++ lib/tweet_streamer.rb
@@ -4,10 +4,10 @@ require File.join(root, 'config', 'environment')
 log = File.join(root, 'log', 'stream.log')
 
 TweetStream.configure do |conf|
-  conf.consumer_key        = 'gWklbzzMP0avmY3VF9FNw'
-  conf.consumer_secret     = 'hNVwweFevkmcMlfiowkhQHczOHaJlURdZoNVMHL80w'
-  conf.oauth_token         = '4618-aAzT8VSxDBLsUSLTOnVEEEFPDukOhfSEstMzkRU6GLE'
-  conf.oauth_token_secret  = 'qAXEKMPi0zFNTMnRJh5ijZj7c8hZSuXWmQ90AH7miZc'
+  conf.consumer_key        = 'a0UmBU7AHn0fFneQ10zYwP0Kk'
+  conf.consumer_secret     = 'JZEqUgKJsTrYjjvYv1wO4jtrNpy50pwoTUwr4UAro3pPxS7Hhw'
+  conf.oauth_token         = '3139928784-B7kPZz6zLAe1M9bTFI29dSfhU6J6Fi9mNdOmhqC'
+  conf.oauth_token_secret  = 'f6cGgOSBU78IPjHz7lDCiOfv4zS4K90OepO2x2355JFqS'
   conf.auth_method         = :oauth
 end
 
